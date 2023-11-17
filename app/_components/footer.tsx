de337969eae56e8af6fc30cbee38d999317df995
Warrapat <warrapat.102@gmail.com>
2023-11-17 12:01:06 +0700

    feat: update footer

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center">
       <p className="text-xs">
-        Copyright © 2023 Warrapat Choedchusakunrat, All rights reserved.{" "}
+        Copyright © 2023 Ltwoz
       </p>
     </footer>
   );
