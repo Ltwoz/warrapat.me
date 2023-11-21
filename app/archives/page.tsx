aff055c94ec174fd78dccc5b8023b7db9af0b6cf
Warrapat <warrapat.102@gmail.com>
2023-11-21 10:47:00 +0700

    feat: update layout margin, padding

--- app/archives/page.tsx
+++ app/archives/page.tsx
@@ -6,7 +6,10 @@ import ArchiveList from "./_components/archive-list";
 export default function ArchivesPage() {
   return (
     <div className="flex flex-col items-start gap-4 w-full">
-      <h1 className="font-bold text-2xl">Archives</h1>
+      <div className="mb-2">
+        <h1 className="font-bold text-2xl">Archives</h1>
+        <p className="text-[#bbbbbb]">Some of the random things i&apos;ve wrote.</p>
+      </div>
       {getArchiveGroups(allArchives).map((group) => (
         <ArchiveList key={group.year} archiveGroup={group} />
       ))}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,7 +4,10 @@ import ProjectCard from "./_components/project-card";
 const ProjectsPage = () => {
   return (
     <div className="flex flex-col items-start gap-4 w-full">
-      <h1 className="font-bold text-2xl">Projects</h1>
+      <div className="mb-2">
+        <h1 className="font-bold text-2xl">Projects</h1>
+        <p className="text-[#bbbbbb]">Here are some of the projects i&apos;ve worked on.</p>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full mt-4">
         {allProjects.map((project) => (
           <ProjectCard key={project._id} project={project} />
